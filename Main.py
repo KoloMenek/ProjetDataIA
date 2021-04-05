fac9b1ae15e581a25095f72a121e66308627e59f
KoloMenek <kolomenek@gmail.com>
2021-04-05 14:38:43 +0200

    avancement

--- Main.py
+++ Main.py
@@ -22,67 +22,86 @@ def canPlay():
 				emptySlots = True
 	return emptySlots
 
+def gameState(theBoard, joueur):
+	gameNotFinished = False
+	if theBoard[0][0] == theBoard[0][1] == theBoard[0][2] != '-':
+		printBoard()
+		print(f"Game Over ! \nLe joueur {joueur} a gagné !")
+		gameNotFinished = True
+	elif theBoard[1][0] == theBoard[1][1] == theBoard[1][2] != '-':
+		printBoard()
+		print(f"Game Over ! \nLe joueur {joueur} a gagné !")
+		gameNotFinished = True
+	elif theBoard[2][0] == theBoard[2][1] == theBoard[2][2] != '-':
+		printBoard()
+		print(f"Game Over ! \nLe joueur {joueur} a gagné !")
+		gameNotFinished = True
+	elif theBoard[0][0] == theBoard[1][0] == theBoard[2][0] != '-':
+		printBoard()
+		print(f"Game Over ! \nLe joueur {joueur} a gagné !")
+		gameNotFinished = True
+	elif theBoard[0][1] == theBoard[1][1] == theBoard[2][1] != '-':
+		printBoard()
+		print(f"Game Over ! \nLe joueur {joueur} a gagné !")
+		gameNotFinished = True
+	elif theBoard[0][2] == theBoard[1][2] == theBoard[2][2] != '-':
+		printBoard()
+		print(f"Game Over ! \nLe joueur {joueur} a gagné !")
+		gameNotFinished = True
+	elif theBoard[0][0] == theBoard[1][1] == theBoard[2][2] != '-':
+		printBoard()
+		print(f"Game Over ! \nLe joueur {joueur} a gagné !")
+		gameNotFinished = True
+	elif theBoard[2][0] == theBoard[1][1] == theBoard[0][2] != '-':
+		printBoard()
+		print(f"Game Over ! \nLe joueur {joueur} a gagné !")
+		gameNotFinished = True
+	elif not canPlay():
+		printBoard()
+		print(f"Game Over ! \nEgalité, personne a gagné !")
+		gameNotFinished = True
+	return gameNotFinished
+
+def minimax(theBoard, isMaximisingPlayer, joueur):
+	isGameFinished = gameState(theBoard, joueur)
+	if isGameFinished:
+		return True
+
+
+
+
+
+
+
 def TicTacToe():
 	wannaPlay = True
 	while wannaPlay:
 		joueur = 'X'
 		compteur = 1
-		gameNotFinished = True
+		gameFinished = False
 
-		while gameNotFinished:
+		while not gameFinished:
 			print(f"Tour {compteur} Joueur: {joueur}")
 			printBoard()
 
 			isPositionOkay = True
-			while isPositionOkay:
-				#Joueur choisi sa position
-				userInput = input("Veuillez selectionner une position i,j (entre 1 et 3) pour valider votre tour !\n")
-				if ',' in userInput:
-					moveI = int(userInput.split(',')[0])
-					moveJ = int(userInput.split(',')[1])
-					if moveI in [1,2,3] and moveJ in [1,2,3]:
-						moveI, moveJ = moveI -1, moveJ -1
-						if theBoard[moveI][moveJ] == '-':
-							isPositionOkay = False
-			theBoard[moveI][moveJ] = joueur
-			compteur += 1
+			if joueur == 'X':
+				while isPositionOkay:
+					#Joueur choisi sa position
+					userInput = input("Veuillez selectionner une position i,j (entre 1 et 3) pour valider votre tour !\n")
+					if ',' in userInput:
+						moveI = int(userInput.split(',')[0])
+						moveJ = int(userInput.split(',')[1])
+						if moveI in [1,2,3] and moveJ in [1,2,3]:
+							moveI, moveJ = moveI -1, moveJ -1
+							if theBoard[moveI][moveJ] == '-':
+								isPositionOkay = False
+				theBoard[moveI][moveJ] = joueur
+				compteur += 1
+				gameFinished = gameState(theBoard,joueur)
 
-			if theBoard[0][0] == theBoard[0][1] == theBoard[0][2] != '-':
-				printBoard()
-				print(f"Game Over ! \nLe joueur {joueur} a gagné !")
-				gameNotFinished = False
-			elif theBoard[1][0] == theBoard[1][1] == theBoard[1][2] != '-':
-				printBoard()
-				print(f"Game Over ! \nLe joueur {joueur} a gagné !")
-				gameNotFinished = False
-			elif theBoard[2][0] == theBoard[2][1] == theBoard[2][2] != '-':
-				printBoard()
-				print(f"Game Over ! \nLe joueur {joueur} a gagné !")
-				gameNotFinished = False
-			elif theBoard[0][0] == theBoard[1][0] == theBoard[2][0] != '-':
-				printBoard()
-				print(f"Game Over ! \nLe joueur {joueur} a gagné !")
-				gameNotFinished = False
-			elif theBoard[0][1] == theBoard[1][1] == theBoard[2][1] != '-':
-				printBoard()
-				print(f"Game Over ! \nLe joueur {joueur} a gagné !")
-				gameNotFinished = False
-			elif theBoard[0][2] == theBoard[1][2] == theBoard[2][2] != '-':
-				printBoard()
-				print(f"Game Over ! \nLe joueur {joueur} a gagné !")
-				gameNotFinished = False
-			elif theBoard[0][0] == theBoard[1][1] == theBoard[2][2] != '-':
-				printBoard()
-				print(f"Game Over ! \nLe joueur {joueur} a gagné !")
-				gameNotFinished = False
-			elif theBoard[2][0] == theBoard[1][1] == theBoard[0][2] != '-':
-				printBoard()
-				print(f"Game Over ! \nLe joueur {joueur} a gagné !")
-				gameNotFinished = False
-			elif not canPlay():
-				printBoard()
-				print(f"Game Over ! \nEgalité, personne a gagné !")
-				gameNotFinished = False
+			else:
+				gameFinished = minimax(theBoard, True, joueur)
 
 			if joueur == 'X':
 				joueur = 'O'
@@ -91,7 +110,7 @@ def TicTacToe():
 		goodInput = True
 		while goodInput:
 			restart = input("Voulez-vous rejouer ? [O/N]\n")
-			if restart in ['O','N']:
+			if restart.upper() in ['O','N']:
 				if restart == 'O':
 					goodInput = False
 				else:
