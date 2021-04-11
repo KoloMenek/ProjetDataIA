c49817931337af3863910007d356e2ff9a82d008
KoloMenek <kolomenek@gmail.com>
2021-04-11 16:57:24 +0200

    jeu sans winning conditions

--- Puissance4/Puissance4.py
+++ Puissance4/Puissance4.py
@@ -2,7 +2,6 @@ import numpy as np
 
 values = {0:'Vide', 1:'Bleu', 2: 'Rouge'}
 theBoard = np.zeros((6,12),dtype=int)
-compteur = 42
 
 def printBoard():
     affichage = "  ─   ─   ─   ─   ─   ─   ─   ─   ─   ─   ─   ─\n"
@@ -23,28 +22,40 @@ def getLastFreeCase(colonne):
             return i
     return -1
 
-def play(colonne, joueur):
-    if canPlay(colonne):
-        ligne = getLastFreeCase(colonne)
-        theBoard[ligne][colonne] = joueur
+def checkWinningConditions(ligne, colonne, compteur):
+    if compteur == 42:
+        print("---TIE---")
         return True
     else:
         return False
 
-def checkWinningConditions(ligne, colonne):
-    if compteur == 0:
-        return "Tie"
-
 def gameLoop():
+    compteur = 1
     joueur = 1
     gameNotFinished = False
     while not gameNotFinished:
-        print(f"Joueur: {joueur}")
-        printBoard()
         isPositionNotOkay = True
+        print(f"Tour: {compteur} Joueur: {joueur}")
+        printBoard()
         while isPositionNotOkay:
-            userInput = input("Veuillez selectionner une colonne entre 1 et 12 pour valider votre tour")
-            move = int(userInput) -1
-            if play(move, joueur):
-                isPositionNotOkay = False
-                
+            userInput = input("Veuillez selectionner une colonne entre 1 et 12 pour valider votre tour:\n")
+            colonne = int(userInput) - 1
+            if colonne in np.arange(0,12):
+                if canPlay(colonne):
+                    ligne = getLastFreeCase(colonne)
+                    theBoard[ligne][colonne] = joueur
+                    compteur += 1
+                    gameNotFinished = checkWinningConditions(ligne, colonne, compteur)
+                    isPositionNotOkay = False
+                else:
+                    print("La case choisi n'est pas valide !")
+            else:
+                print("La case choisi n'est pas valide !")
+
+        if joueur == 1:
+            joueur = 2
+        else:
+            joueur = 1
+
+if __name__ == "__main__":
+    gameLoop()
