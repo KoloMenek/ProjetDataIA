deb812cdf65e5157c1c3144b9d820a38d033f977
Mego93 <thierryvo2@yahoo.fr>
2021-04-29 17:43:48 +0200

    Update Puissance4.py

--- Puissance4/Puissance4.py
+++ Puissance4/Puissance4.py
@@ -2,9 +2,11 @@ import numpy as np
 
 nb_lignes = 6
 nb_colonnes = 12
+compteur = 0
 
 values = {0:'Vide', 1:'Bleu', 2: 'Rouge'}
 theBoard = np.zeros((nb_lignes,nb_colonnes),dtype=int)
+
 
 def printBoard():
     affichage = "  ─   ─   ─   ─   ─   ─   ─   ─   ─   ─   ─   ─\n"
@@ -26,90 +28,84 @@ def getLastFreeCase(colonne):
     return -1
 
 
-def heuristique(ligne,colonne,joueur):
-    '''exemple d'heuristique : calcule le nombre d'endoit où le joueur peut gagner moins le nombre d'endroits où l'adversaire peut gagner '''
+def heuristique(joueur):
     heur = 0
     #teste toutes les possibilités en lignes
-    i = ligne
-    j = colonne
-    if((ligne > nb_lignes-1 or ligne < 0) or (colonne > nb_colonnes-1 or colonne < 0)):
-        return 
-    else:
-        for i in range(6):
-            for j in range(9):
-                if theBoard[i][j] != 1 and theBoard[i][j+1] != 1 and theBoard[i][j+2] != 1 and theBoard[i][j+3] != 1:
-                    heur +=1
-                if theBoard[i][j] != 2 and theBoard[i][j+1] != 2 and theBoard[i][j+2] != 2 and theBoard[i][j+3] != 2:
-                    heur -=1
-    
-    
-        #teste toutes les possibilités en colonnes
-        for j in range(12):
-            for i in range(3):
-                if theBoard[i][j] != 1 and theBoard[i+1][j] != 1 and theBoard[i+2][j] != 1 and theBoard[i+3][j] != 1:
-                    heur +=1
-                if theBoard[i][j] != 2 and theBoard[i+1][j] != 2 and theBoard[i+2][j] != 2 and theBoard[i+3][j] != 2:
-                    heur -=1
-    
-        #teste toutes les possibilités en diagonales montantes / et descendantes \
+    for i in range(6):
+        for j in range(9):
+            if theBoard[i][j] != 1 and theBoard[i][j+1] != 1 and theBoard[i][j+2] != 1 and theBoard[i][j+3] != 1:
+                heur +=1
+            if theBoard[i][j] != 2 and theBoard[i][j+1] != 2 and theBoard[i][j+2] != 2 and theBoard[i][j+3] != 2:
+                heur -=1
+
+
+    #teste toutes les possibilités en colonnes
+    for j in range(12):
         for i in range(3):
-            for j in range(9):
-                if theBoard[i][j] != 1 and theBoard[i+1][j+1] != 1 and theBoard[i+2][j+2] != 1 and theBoard[i+3][j+3] != 1:
-                    heur +=1
-                if theBoard[i+3][j] != 1 and theBoard[i+2][j+1] != 1 and theBoard[i+1][j+2] != 1 and theBoard[i][j+3] != 1:
-                    heur +=1
-                if theBoard[i][j] != 2 and theBoard[i+1][j+1] != 2 and theBoard[i+2][j+2] != 2 and theBoard[i+3][j+3] != 2:
-                    heur -=1
-                if theBoard[i+3][j] != 2 and theBoard[i+2][j+1] != 2 and theBoard[i+1][j+2] != 2 and theBoard[i][j+3] != 2:
-                    heur -=1
+            if theBoard[i][j] != 1 and theBoard[i+1][j] != 1 and theBoard[i+2][j] != 1 and theBoard[i+3][j] != 1:
+                heur +=1
+            if theBoard[i][j] != 2 and theBoard[i+1][j] != 2 and theBoard[i+2][j] != 2 and theBoard[i+3][j] != 2:
+                heur -=1
+
+    #teste toutes les possibilités en diagonales montantes / et descendantes \
+    for i in range(3):
+        for j in range(9):
+            if theBoard[i][j] != 1 and theBoard[i+1][j+1] != 1 and theBoard[i+2][j+2] != 1 and theBoard[i+3][j+3] != 1:
+                heur +=1
+            if theBoard[i+3][j] != 1 and theBoard[i+2][j+1] != 1 and theBoard[i+1][j+2] != 1 and theBoard[i][j+3] != 1:
+                heur +=1
+            if theBoard[i][j] != 2 and theBoard[i+1][j+1] != 2 and theBoard[i+2][j+2] != 2 and theBoard[i+3][j+3] != 2:
+                heur -=1
+            if theBoard[i+3][j] != 2 and theBoard[i+2][j+1] != 2 and theBoard[i+1][j+2] != 2 and theBoard[i][j+3] != 2:
+                heur -=1
 
     return heur
 
-def minmax(board,alpha,beta, profondeur, isMaximazing,joueur):
-    gameNotFinished = checkWinningConditions(ligne, colonne, compteur,joueur)
-    if (profondeur == 0):
-        return scores[result]
-
-    if isMaximazing:       
-        bestScore = 999999
-        for colonne in np.arange(0,12):
-            if canPlay(colonne):
-                ligne = getLastFreeCase(colonne)
-                theBoard[ligne][colonne] = joueur
-                score = minmax(theBoard,np.NINF,np.Inf, 4 ,False)
-                theBoard[ligne][colonne] = 0
-                if score > bestScore:
-                    bestScore = score
-                    move = (ligne,colonne)
-        return bestScore
+def minmax(board,alpha,beta, profondeur, isMaximazing,ligne,colonne,joueur):
+    if(profondeur == 0 or checkWinningConditions(ligne,colonne,joueur)):
+        return heuristique()
     
-    else:
-        bestScore = 999999
+    if(isMaximazing):
+        maxEvaluate = np.NINF
         for colonne in np.arange(0,12):
             if canPlay(colonne):
                 ligne = getLastFreeCase(colonne)
-                theBoard[ligne][colonne] = joueur
-                score = minmax(theBoard,np.NINF,np.Inf, 4 ,False)
+                theBoard[ligne][colonne] = 2
+                evaluate = minmax(theBoard,alpha,beta, profondeur-1 ,False,ligne,colonne,1)
                 theBoard[ligne][colonne] = 0
-                if score > bestScore:
-                    bestScore = score
-                    move = (ligne,colonne)
-        return bestScore
+                maxEvaluate = max(maxEvaluate,evaluate)
+                alpha = max(alpha,evaluate)
+                if beta <= alpha:
+                    break
+        return maxEvaluate
+    else:
+        minEvaluate = np.Inf
+        for colonne in np.arange(0,12):
+            if canPlay(colonne):
+                ligne = getLastFreeCase(colonne)
+                theBoard[ligne][colonne] = 1
+                evaluate = minmax(theBoard,alpha,beta, profondeur-1 ,False,ligne,colonne,2)
+                theBoard[ligne][colonne] = 0
+                minEvaluate = min(minEvaluate,evaluate)
+                beta = min(beta,evaluate)
+                if beta <= alpha:
+                    break            
+        return minEvaluate
 
-def bestMove(joueur):
+def turnAI():
 	#AI to make its turn
     move = (0,0)
     bestScore = np.NINF
     for colonne in np.arange(0,12):
         if canPlay(colonne):
             ligne = getLastFreeCase(colonne)
-            theBoard[ligne][colonne] = joueur
-            score = minmax(theBoard,np.NINF,np.Inf, 4 ,False)
+            theBoard[ligne][colonne] = 2
+            score = minmax(theBoard,np.NINF,np.Inf, 4 ,False,ligne,colonne,2)
             theBoard[ligne][colonne] = 0
             if score > bestScore:
                 bestScore = score
                 move = (ligne,colonne)
-    theBoard[move[0]][move[1]] = joueur
+    theBoard[move[0]][move[1]] = 2
 
 def parcours(ligne,colonne,Vx,Vy,joueur):
     cpt = 0   
@@ -127,10 +123,10 @@ def parcours(ligne,colonne,Vx,Vy,joueur):
 
 # Le principe est qu'on vérifie le conformité de la pièce du joueur avec celle suivante
 # suivant l'axe, si on trouve une pièce adverse on s'arrete et on passe à l'autre axe, sinon on incrémente le compteur
-def checkWinningConditions(ligne,colonne,compteur,joueur):
+def checkWinningConditions(ligne,colonne,joueur):
     winning = False
     if(compteur==72):
-        print("__TIE__")
+        print("Egalité")
         winning = None
     victory = 4
     
@@ -160,96 +156,13 @@ def checkWinningConditions(ligne,colonne,compteur,joueur):
        
     return winning
 
-    """
-    # A améliorer, utiliser une seule fonction
-    # Ligne -
-    cpt_ligne = 1
-    for i in range(colonne+1,nb_colonnes): # out of range
-        if theBoard[ligne][i] == joueur:
-            cpt_ligne+=1
-        else:
-            break
-    for i in range(colonne-1,-1,-1):
-        if theBoard[ligne][i] == joueur:
-            cpt_ligne+=1
-        else:
-            break
-    
-    # Colonne |
-    cpt_colonne = 1
-    for i in range(ligne+1,nb_lignes):
-        if theBoard[i][colonne] == joueur:
-            cpt_colonne+=1
-        else:
-            break
-    for i in range(ligne-1,-1,-1):
-        if theBoard[i][colonne] == joueur:
-            cpt_colonne+=1
-        else:
-            break   
-        
-    # Diagonale ascendante /
-    cpt_diagAsc = 1
-    cptLoopAsc_1 = 1
-    cptLoopAsc_2 = 1
-    for i in range(colonne+1,nb_colonnes):
-        # On dépasse le board (supérieur)
-        if(ligne-cptLoopAsc_1 < 0):
-            break
-        if theBoard[ligne-cptLoopAsc_1][i] == joueur:
-            cpt_diagAsc+=1
-        else:
-            break
-        cptLoopAsc_1+=1
-        
-    for i in range(colonne-1,-1,-1):
-        # On dépasse le board (inférieur)
-        if(ligne+cptLoopAsc_2 > nb_lignes-1):
-            break
-        if theBoard[ligne+cptLoopAsc_2][i] == joueur:
-            cpt_diagAsc+=1
-        else:
-            break   
-        cptLoopAsc_2+=1
-    
-    #diagonale descendante \ 
-    cpt_diagDesc = 1
-    cptLoopDesc_1 = 1
-    cptLoopDesc_2 = 1
-    for i in range(colonne+1,nb_colonnes):
-        # On dépasse le board (supérieur)
-        if(ligne+cptLoopDesc_1 > nb_lignes-1):
-            break
-        if theBoard[ligne+cptLoopDesc_1][i] == joueur:
-            cpt_diagDesc+=1
-        else:
-            break
-        cptLoopDesc_1+=1
-    for i in range(colonne-1,-1,-1):
-        # On dépasse le board (inférieur)
-        print(ligne+cptLoopDesc_2 )
-        if(ligne+cptLoopDesc_2 < 0):
-            break
-        if theBoard[ligne-cptLoopDesc_2][i] == joueur:
-            cpt_diagDesc+=1
-        else:
-            break  
-        cptLoopDesc_2+=1
-    #print("cpt_colonne:",cpt_colonne, "cpt_ligne:",cpt_ligne,"cpt_diagDesc:",cpt_diagDesc,'cpt_diagAsc:',cpt_diagAsc)
-    if cpt_colonne >= victory or cpt_ligne >= victory or cpt_diagAsc >= victory or cpt_diagDesc >= victory:
-        return True
-    else:
-        return False
-    """
-
-
 def PvP():
-    compteur = 1
     joueur = 1
+    compteur =1
     gameNotFinished = False
     while not gameNotFinished:
         isPositionNotOkay = True
-        print(f"Tour: {compteur} Joueur: {joueur}")
+        print("Tour: "+ str(compteur)+f" Joueur: {joueur}")
         printBoard()
         while isPositionNotOkay:
             userInput = input("Veuillez selectionner une colonne entre 1 et 12 pour valider votre tour:\n")
@@ -259,7 +172,7 @@ def PvP():
                     ligne = getLastFreeCase(colonne)
                     theBoard[ligne][colonne] = joueur
                     compteur += 1
-                    gameNotFinished = checkWinningConditions(ligne, colonne, compteur,joueur)
+                    gameNotFinished = checkWinningConditions(ligne, colonne,joueur)
                     isPositionNotOkay = False
                 else:
                     print("La case choisi n'est pas valide !")
@@ -305,17 +218,17 @@ def PvIA():
                         ligne = getLastFreeCase(colonne)
                         theBoard[ligne][colonne] = joueur
                         compteur += 1
-                        gameNotFinished = checkWinningConditions(ligne, colonne, compteur,joueur)
+                        gameNotFinished = checkWinningConditions(ligne, colonne,joueur)
                         isPositionNotOkay = False
                     else:
                         print("Il n'est pas possible de jouer là !")
                 else:
                     print("Case hors du plateau !")
         else:
-            printBoard(joueur)
-            bestMove()
+            printBoard()
+            turnAI()
             compteur += 1
-            gameNotFinished = checkWinningConditions(ligne, colonne, compteur,joueur)
+            gameNotFinished = checkWinningConditions(ligne, colonne,joueur)
 
 
         if gameNotFinished is True:
